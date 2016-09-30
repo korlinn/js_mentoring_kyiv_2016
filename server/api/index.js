704ca4c6786f94422a0fb6060fed0cc04c3e71e1
Natalia_Ivanova <natalia_ivanova@epam.com>
2016-09-30 19:18:31 +0300

    remove unused module

--- server/api/index.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-
-const express = require('express');
-const picture = require('./picture');
-const router = express.Router();
-
-module.exports = router;
--- server/api/picture/index.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-
-const express = require('express');
-const controller = require('./picture.controller');
-const router = express.Router();
-
-module.exports = router;
