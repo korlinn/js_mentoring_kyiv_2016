ec13f41578c021af344cd89d8469c832566b9744
Natalya Ivanova <Natalia_Ivanova@epam.com>
2016-11-01 01:51:21 +0200

    HW5: page-not-found component added

---
+++ src/angular/app/common/page-not-found/page-not-found.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'page-not-found',
+  templateUrl: '/angular/app/common/page-not-found/page-not-found.component.html',
+})
+export class PageNotFoundComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
